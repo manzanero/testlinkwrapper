27b12e5cf4588af28d20cd0975f5ea7e3d4dbf1f
alejandro.manzanero <SJHD3jo*Kx>
2020-01-10 14:19:37 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
-from distutils.core import setup
+from setuptools import setup
 
-# read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='testlinkwrapper',
     packages=['testlinkwrapper'],
-    version='0.1.5',
+    version='0.1.6',
     license='MIT',
     description='Wrapper for test tool Testlink',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Alejandro Manzanero',
-    # author_email='your.email@domain.com',
-    # url='https://github.com/user/reponame',
-    download_url='https://github.com/Manzanero/testlinkwrapper/archive/v_0_1_5.tar.gz',
+    author_email='alejmans@domain.com',
+    url='https://github.com/Manzanero/testlinkwrapper',
+    download_url='https://github.com/Manzanero/testlinkwrapper/archive/v_0_1_6.tar.gz',
     keywords=['test', 'testlink', 'wrapper'],
     install_requires=[
         'TestLink-API-Python-client',
